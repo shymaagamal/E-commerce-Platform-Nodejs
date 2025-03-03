a6bfa6a2c3205b848ab50544952f8ca39e4c5175
Nagham Mohamed <naghamfakhreldin@gmail.com>
2025-03-03 20:48:49 +0200

    feat(logs): Adding the 2 order service logs files for loggining either error or success messages for each part in the order service

--- routes/orders-routes.js
+++ routes/orders-routes.js
@@ -6,6 +6,7 @@ const orderRouter = express.Router();
 // Orders Routes
 // orderRouter.post('/', placeOrder);
 orderRouter.get('/', getOrderHistory);
-// orderRouter.get('/:id', getOrderById);
+orderRouter.get('/:id', getOrderById);
+orderRouter.post('/payment_checkout', getOrderById);
 
 export default orderRouter;
