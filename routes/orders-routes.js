72d474e4231ae89798947057df5bbb4156bcf9fe
Nagham Mohamed <107771328+NaghamMohamedMohamed@users.noreply.github.com>
2025-03-03 20:53:36 +0200

    fix(orders-routes.js): Remove uninitialized routes

--- routes/orders-routes.js
+++ routes/orders-routes.js
@@ -6,7 +6,5 @@ const orderRouter = express.Router();
 // Orders Routes
 // orderRouter.post('/', placeOrder);
 orderRouter.get('/', getOrderHistory);
-orderRouter.get('/:id', getOrderById);
-orderRouter.post('/payment_checkout', getOrderById);
 
 export default orderRouter;
