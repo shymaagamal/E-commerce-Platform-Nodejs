4e72070938fc8e9af7ac9e5728514f4f04968396
Nagham Mohamed <naghamfakhreldin@gmail.com>
2025-03-04 23:54:56 +0200

    feat(.env): Adding the stripe secret key for requests authentication for stripe ( Online Payment Method )

--- routes/orders-routes.js
+++ routes/orders-routes.js
@@ -2,6 +2,10 @@ import express from 'express';
 import {getOrderById, getOrdersHistory} from '../controllers/order-controller.js';
 import {verifyToken} from '../middleware/auth-middleware.js';
 import {morganMiddleware} from '../middleware/morgan-middleware.js';
+import {getOrdersHistory , getOrderById } from '../controllers/order-controller.js';
+import { verifyToken } from '../middleware/auth-middleware.js';
+import { orderPayment } from "../controllers/paymentController.js";
+
 
 const orderRouter = express.Router();
 orderRouter.use(morganMiddleware);
@@ -9,5 +13,10 @@ orderRouter.use(morganMiddleware);
 // Orders Routes
 orderRouter.get('/', verifyToken, getOrdersHistory);
 orderRouter.get('/:id', verifyToken, getOrderById);
+
+orderRouter.get('/cancel/:id', verifyToken , );
+
+// Order Payment Checkout Route
+orderRouter.post('/payment/:id', verifyToken , orderPayment);
 
 export default orderRouter;
